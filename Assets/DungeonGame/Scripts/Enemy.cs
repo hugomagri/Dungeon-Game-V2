54821d1a519df7522e0061ec1613c28d0be2cac7
hugo <hugomagrimariutti07@gmail.com>
2025-09-03 12:04:43 -0300

    AULA1

---
+++ Assets/DungeonGame/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string nome;
+    public float vida;
+    public float maxVida;
+    public float speed;
+    public Transform target;
+
+    private void Start()
+    {
+        vida = maxVida;
+        Introduction();
+
+    }
+
+    void Introduction()
+    {
+        Debug.Log("O nome é " + nome + " a vida é " + vida + " e o maximo de vida é " + maxVida);
+    }
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position, 
+            target.position,
+            speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+
+}
--- Assets/DungeonGame/Scripts/PlayerMovement.cs
+++ Assets/DungeonGame/Scripts/PlayerMovement.cs
@@ -8,18 +8,29 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private float moveH, moveV;
     public Animator animator;
-
+    public float speed = 2f;
     private void Start()
     {
         //Ao iniciar o jogo, acessa o Rigidbody2D do personagem
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = new Vector3(0,0,0);
+        }
+    }
 
     private void FixedUpdate()
     {
-              
 
+
+        moveH = Input.GetAxis("Horizontal") * speed;
+        moveV = Input.GetAxis("Vertical") * speed;
+
+        rb.linearVelocity = new Vector2(moveH, moveV);
 
         // Responsável por ativar as animações
         animator.SetFloat("Horizontal", moveH);
